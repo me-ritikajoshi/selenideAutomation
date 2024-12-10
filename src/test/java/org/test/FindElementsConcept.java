aa73007ea6a3e83d87134b863e7e309f5e2c700d
me-ritikajoshi <me.ritikajoshi@gmail.com>
2024-12-10 11:26:42 +0545

    Changes in files

--- src/test/java/org/test/FindElementsConcept.java
+++ src/test/java/org/test/FindElementsConcept.java
@@ -3,6 +3,7 @@ package org.test;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
@@ -19,12 +20,16 @@ public class FindElementsConcept {
 		ElementsCollection colls= $$(By.tagName("a"));
 		System.out.println(colls.size());
 		
-		for(SelenideElement e : colls) {
-			String text=e.getText();
-			String href=e.getAttribute("href");
-			System.out.println(text + "----" + href);
-		}
+//		for(SelenideElement e : colls) {
+//			String text=e.getText();
+//			String href=e.getAttribute("href");
+//			System.out.println(text + "----" + href);
+//		}
+		
+		colls.shouldHave(CollectionCondition.sizeLessThan(100));
+//		colls.forEach(e -> System.out.println(e.getText()));
+		colls.stream().filter(e -> !e.getText().isEmpty()).forEach(e -> System.out.println(e.getText()));
+		colls.last(10).forEach(e -> System.out.println(e.getText()));
 		
 	}
-
 }
