64b8319d12838a00b2483e0fb2d045cd3aa804af
me-ritikajoshi <me.ritikajoshi@gmail.com>
2024-12-10 11:01:30 +0545

    Changes in files

--- src/test/java/org/test/FindElementsConcept.java
+++ src/test/java/org/test/FindElementsConcept.java
@@ -1,14 +1,22 @@
 package org.test;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+
+import com.codeborne.selenide.ElementsCollection;
+
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
 
 
 
 public class FindElementsConcept {
 	@Test
 	public void elementsTest() {
-		open("Https://www.amazon.com/");
+		open("https://www.amazon.com/");
+		ElementsCollection colls= $$(By.tagName("a"));
+		System.out.println(colls.size());
 		
 	}
 
