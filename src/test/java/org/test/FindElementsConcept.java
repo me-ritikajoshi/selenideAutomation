d82d988d2bac4ba2ba1e07d542cf0a0830548b46
me-ritikajoshi <me.ritikajoshi@gmail.com>
2024-12-10 11:05:33 +0545

    Changes in files

--- src/test/java/org/test/FindElementsConcept.java
+++ src/test/java/org/test/FindElementsConcept.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
@@ -14,9 +15,15 @@ import static com.codeborne.selenide.Condition.*;
 public class FindElementsConcept {
 	@Test
 	public void elementsTest() {
-		open("https://www.amazon.com/");
+		open("https://selenide.org/documentation.html");
 		ElementsCollection colls= $$(By.tagName("a"));
 		System.out.println(colls.size());
+		
+		for(SelenideElement e : colls) {
+			String text=e.getText();
+			String href=e.getAttribute("href");
+			System.out.println(text + "----" + href);
+		}
 		
 	}
 
