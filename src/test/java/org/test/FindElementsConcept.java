854bdb0f37c485d4aee3889f0908b1db6a403143
me-ritikajoshi <me.ritikajoshi@gmail.com>
2024-12-10 10:57:55 +0545

    Changes in files

---
+++ src/test/java/org/test/FindElementsConcept.java
@@ -0,0 +1,15 @@
+package org.test;
+
+import org.testng.annotations.Test;
+import static com.codeborne.selenide.Selenide.*;
+
+
+
+public class FindElementsConcept {
+	@Test
+	public void elementsTest() {
+		open("Https://www.amazon.com/");
+		
+	}
+
+}
